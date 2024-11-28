72add02308df824ea3292d8674a5a2fd0f44a07c
MS5682 <mss5682@naver.com>
2024-11-28 17:53:01 +0900

    만료된 메시지 삭제

--- webchat/src/main/java/com/sms/webchat/WebchatApplication.java
+++ webchat/src/main/java/com/sms/webchat/WebchatApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableWebSocket
 @EnableJpaAuditing
+@EnableScheduling
 public class WebchatApplication {
 
     public static void main(String[] args) {
--- webchat/src/main/java/com/sms/webchat/scheduler/MessageCleanupScheduler.java
+++ webchat/src/main/java/com/sms/webchat/scheduler/MessageCleanupScheduler.java
@@ -1,9 +1,9 @@
 package com.sms.webchat.scheduler;
 
 import java.time.LocalDateTime;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sms.webchat.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +13,8 @@ import lombok.RequiredArgsConstructor;
 public class MessageCleanupScheduler {
     private final MessageRepository messageRepository;
     
-    @Scheduled(cron = "0 0 2 * * *")  // 매일 새벽 2시에 실행
+    @Scheduled(cron = "0 0 2 * * *")
+    @Transactional
     public void cleanupExpiredMessages() {
         messageRepository.deleteAllByExpiresAtBefore(LocalDateTime.now());
     }
